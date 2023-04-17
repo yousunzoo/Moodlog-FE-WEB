d1b7ee3032441621b0414ca1cb421a3a8889561a
annoeul <shdmf1137@gmail.com>
2023-04-17 22:00:11 +0900

    feat: Component 추가

--- src/components/loginForm/index.tsx
+++ src/components/loginForm/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Button, Container, Input, Title, Form } from './style'
 
 function LoginForm() {
-  return <div></div>
+  return (
+    <Form>
+      <Title>MoodLog</Title>
+      <Input type="email" placeholder="Email" />
+      <Input type="password" placeholder="Password" />
+      <Button type="submit">Login</Button>
+    </Form>
+  )
 }
 
 export default LoginForm
