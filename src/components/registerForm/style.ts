cb3fcf0d03717c580738af6eb5bd98303c7e0d24
dokimion24 <leecy3610@gmail.com>
2023-04-17 22:47:58 +0900

    design: style 추가

--- src/components/registerForm/style.ts
+++ src/components/registerForm/style.ts
@@ -1,12 +1,37 @@
 import styled from 'styled-components'
 
-export const InputWrpper = styled.div`
+export const CloseButton = styled.button`
+  width: 16px;
+  height: 16px;
+  background-image: url('../../../public/assets/icons/close.png');
+  background-size: cover;
+`
+
+export const InputWrapper = styled.div`
   width: 100%;
+  height: 80px;
 `
 
 export const Input = styled.input`
   width: 100%;
+  height: 50px;
   border-radius: 10px;
+  background-color: #f8f8f8;
+  border: 1px solid #959595;
 `
 
 export const Text = styled.div``
+
+export const ResigterButton = styled.button`
+  width: 100%;
+  background-color: #998c84;
+  border-radius: 10px;
+  height: 50px;
+  color: #fff;
+`
+export const ProfileContainer = styled.div`
+  display: flex;
+  margin: 30px 0;
+`
+
+export const ProfileTextContainer = styled.div``
--- src/pages/registerPage/style.ts
+++ src/pages/registerPage/style.ts
@@ -3,3 +3,11 @@ import styled from 'styled-components'
 const Container = styled.div`
   padding: 20px 10px;
 `
+
+export const Logo = styled.div`
+  width: 200px;
+  height: 200px;
+  margin: auto;
+  background-image: url('../../../public/assets/icons/logo.png');
+  background-size: cover;
+`
