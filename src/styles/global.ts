9d3dae44a1df09848541ea1a4f6620906e2cf8e6
dokimion24 <leecy3610@gmail.com>
2023-04-17 17:33:15 +0900

    design: 페이지 가운데 정렬

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,5 +2,14 @@ import { createGlobalStyle } from 'styled-components'
 import { normalize } from 'styled-normalize'
 
 export const GlobalStyle = createGlobalStyle`
-${normalize}
+  ${normalize}
+
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    display: flex;
+    justify-content: center;
+  }
 `
