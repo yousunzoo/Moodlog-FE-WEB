9116e7928a449fe0a6863067bf9c672b6839336a
dokimion24 <leecy3610@gmail.com>
2023-04-17 15:57:52 +0900

    feat: ThemeProvider, GlobalStyles 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { GlobalStyle } from './styles/global'
+import { theme } from './styles/theme'
+import { ThemeProvider } from 'styled-components'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
     <App />
-  </React.StrictMode>,
+  </ThemeProvider>,
 )
