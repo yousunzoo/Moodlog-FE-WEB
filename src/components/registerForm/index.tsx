141d21650e6bc3e0e15d8c859af8808bd8d2ec1a
dokimion24 <leecy3610@gmail.com>
2023-04-17 22:48:27 +0900

    feat: 프로필, 회원가입 버튼 레이아웃 추가

--- src/components/registerForm/index.tsx
+++ src/components/registerForm/index.tsx
@@ -5,25 +5,30 @@ import Profile from '../common/profile'
 function RegisterForm() {
   return (
     <>
-      <S.InputWrpper>
+      <S.InputWrapper>
         <S.Input type="text" placeholder="사용하실 이메일을 입력해주세요" />
         <S.Text>검증 메세지 출력</S.Text>
-      </S.InputWrpper>
-      <S.InputWrpper>
+      </S.InputWrapper>
+      <S.InputWrapper>
         <S.Input type="text" placeholder="사용하실 사용자명을 입력해주세요" />
         <S.Text>검증 메세지 출력</S.Text>
-      </S.InputWrpper>
-      <S.InputWrpper>
+      </S.InputWrapper>
+      <S.InputWrapper>
         <S.Input type="text" placeholder="사용하실 비밀번호를을 입력해주세요" />
         <S.Text>검증 메세지 출력</S.Text>
-      </S.InputWrpper>
-      <S.InputWrpper>
+      </S.InputWrapper>
+      <S.InputWrapper>
         <S.Input type="text" placeholder="비밀번호를 다시 한 번 입력해주세요" />
         <S.Text>검증 메세지 출력</S.Text>
-      </S.InputWrpper>
-      <div>
+      </S.InputWrapper>
+      <S.ProfileContainer>
         <Profile />
-      </div>
+        <S.ProfileTextContainer>
+          <button>프로필 이미지 등록</button>
+          <div>파일이름</div>
+        </S.ProfileTextContainer>
+      </S.ProfileContainer>
+      <S.ResigterButton>회원가입</S.ResigterButton>
     </>
   )
 }
