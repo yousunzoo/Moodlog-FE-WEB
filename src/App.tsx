0c1aebf91f20619b380d4cc52d48a69c1f9a0635
dokimion24 <leecy3610@gmail.com>
2023-04-17 15:59:25 +0900

    feat: queryClientProvider, router, ReactQueryDevtools 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { QueryClientProvider } from 'react-query'
+
+import { queryClient } from './utils/queryClient'
+import Router from './routes/Router'
 
 function App() {
-  return <div></div>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  )
 }
 
 export default App
