1599617825c9c37971dd1df1785479bea4459f5d
dokimion24 <leecy3610@gmail.com>
2023-04-17 20:16:42 +0900

    feat: 회원가입 레이아웃 1차 구현

--- src/components/registerForm/index.tsx
+++ src/components/registerForm/index.tsx
@@ -1,15 +1,31 @@
 import React from 'react'
 import * as S from './style'
+import Profile from '../common/profile'
 
-function registerForm() {
+function RegisterForm() {
   return (
     <>
-      <S.Input type="text" placeholder="사용하실 이메일을 입력해주세요" />
-      <S.Input type="text" placeholder="사용하실 사용자명을 입력해주세요" />
-      <S.Input type="text" placeholder="사용하실 비밀번호를을 입력해주세요" />
-      <S.Input type="text" placeholder="비밀번호를 다시 한 번 입력해주세요" />
+      <S.InputWrpper>
+        <S.Input type="text" placeholder="사용하실 이메일을 입력해주세요" />
+        <S.Text>검증 메세지 출력</S.Text>
+      </S.InputWrpper>
+      <S.InputWrpper>
+        <S.Input type="text" placeholder="사용하실 사용자명을 입력해주세요" />
+        <S.Text>검증 메세지 출력</S.Text>
+      </S.InputWrpper>
+      <S.InputWrpper>
+        <S.Input type="text" placeholder="사용하실 비밀번호를을 입력해주세요" />
+        <S.Text>검증 메세지 출력</S.Text>
+      </S.InputWrpper>
+      <S.InputWrpper>
+        <S.Input type="text" placeholder="비밀번호를 다시 한 번 입력해주세요" />
+        <S.Text>검증 메세지 출력</S.Text>
+      </S.InputWrpper>
+      <div>
+        <Profile />
+      </div>
     </>
   )
 }
 
-export default registerForm
+export default RegisterForm
