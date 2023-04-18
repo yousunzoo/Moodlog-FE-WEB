2366d7fc6b20465f4680bc8845ac412e5c561679
annoeul <shdmf1137@gmail.com>
2023-04-18 20:21:09 +0900

    feat : Signup 컴포넌트 추가

--- src/components/loginForm/index.tsx
+++ src/components/loginForm/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import { Button, Container, Input, Title, Form } from './style'
+import { Button, Container, Input, Title, Form, Signup, Span } from './style'
 
 function LoginForm() {
   return (
     <Form>
       <Title>MoodLog</Title>
-      <Input type="email" placeholder="Email" />
-      <Input type="password" placeholder="Password" />
-      <Button type="submit">Login</Button>
+      <Input type="email" placeholder="사용자 이메일" />
+      <Input type="password" placeholder="비밀번호" />
+      <Button type="submit">로그인</Button>
+      <Signup>
+        아직 회원이 아니신가요? <Span>회원가입</Span>
+      </Signup>
     </Form>
   )
 }
