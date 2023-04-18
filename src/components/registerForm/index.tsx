2c49d85c0ede83b39dfdea2f513001005ee7cc5f
dokimion24 <leecy3610@gmail.com>
2023-04-18 21:02:17 +0900

    feat: 회원가입 api 요청

--- src/components/registerForm/index.tsx
+++ src/components/registerForm/index.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as S from './style'
 import Profile from '../common/profile'
+import { axiosInstance } from '../../apis/axios'
+import { NewUser } from '../../types/'
 
 function RegisterForm() {
+  const [user, setUser] = useState()
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const userObj = {
+      email: 'test1@test.com',
+      password: '12341234',
+      username: 'test1',
+      profile_image: '../../public/assets/icons/close.png',
+    }
+    const res = await axiosInstance.post('/auth/register', { data: userObj })
+    console.log(res)
+  }
+
   return (
-    <>
+    <S.Form onSubmit={handleSubmit}>
       <S.InputWrapper>
         <S.Input type="text" placeholder="사용하실 이메일을 입력해주세요" />
         <S.Text>검증 메세지 출력</S.Text>
@@ -28,8 +44,8 @@ function RegisterForm() {
           <div>파일이름</div>
         </S.ProfileTextContainer>
       </S.ProfileContainer>
-      <S.ResigterButton>회원가입</S.ResigterButton>
-    </>
+      <S.ResigterButton type="submit">회원가입</S.ResigterButton>
+    </S.Form>
   )
 }
 
