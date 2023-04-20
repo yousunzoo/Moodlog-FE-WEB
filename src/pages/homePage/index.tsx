d4139db6585c14781fe2dd2150e831f9198b42c5
yousunzoo <dpfvmffjqm@gmail.com>
2023-04-20 21:46:44 +0900

    feat: 공통적으로 사용할 Modal 컴포넌트 생성
    안에 들어갈 내용들은 children props로 관리

--- src/pages/homePage/index.tsx
+++ src/pages/homePage/index.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { useState } from 'react'
+import Modal from '../../components/common/modal'
 
 function HomePage() {
-  return <div>HomePage</div>
+  const [isOpen, setIsOpen] = useState(false)
+  const handleOpenModal = () => {
+    setIsOpen(true)
+  }
+
+  const handleCloseModal = () => {
+    setIsOpen(false)
+  }
+
+  return (
+    <div>
+      <button onClick={handleOpenModal}>click</button>
+      <Modal isOpen={isOpen} onClose={handleCloseModal}>
+        // 모달 내용
+      </Modal>
+    </div>
+  )
 }
 
 export default HomePage
