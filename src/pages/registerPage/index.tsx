8ef585be0b09cbe70e26f1f32aaa4a6b3811f1b6
dokimion24 <leecy3610@gmail.com>
2023-04-20 22:29:48 +0900

    feat: nav 컴포넌트 추가

--- src/pages/registerPage/index.tsx
+++ src/pages/registerPage/index.tsx
@@ -3,6 +3,7 @@ import RegisterForm from '../../components/registerForm'
 import * as S from './style'
 import CloseButton from '../../components/common/button/closeButton'
 import Logo from '../../components/common/logo'
+import Nav from '../../components/common/Nav'
 
 function RegisterPage() {
   return (
@@ -10,6 +11,7 @@ function RegisterPage() {
       <CloseButton />
       <Logo size={200} />
       <RegisterForm />
+      <Nav />
     </div>
   )
 }
