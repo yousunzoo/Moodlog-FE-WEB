40a7cb2c055921f492129e6ae83abc175257b9fe
dokimion24 <leecy3610@gmail.com>
2023-04-20 21:20:59 +0900

    feat: // @ts-ignore 제거

--- src/components/common/profile/index.tsx
+++ src/components/common/profile/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import * as S from './style'
 
-// @ts-ignore
 function Profile({ img = '../../../public/assets/icons/profile.png' }) {
   console.log(img)
   return <S.ProfileImg src={img} />
