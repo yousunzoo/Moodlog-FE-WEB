5a72d236423e57ad5f50fb5fb14cb4e2737cf8b7
dokimion24 <leecy3610@gmail.com>
2023-04-20 21:44:36 +0900

    feat: Logo 컴포넌트 추가

--- src/pages/registerPage/index.tsx
+++ src/pages/registerPage/index.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import RegisterForm from '../../components/registerForm'
 import * as S from './style'
 import CloseButton from '../../components/common/button/closeButton'
+import Logo from '../../components/common/logo'
 
 function RegisterPage() {
   return (
     <div>
       <CloseButton />
-      <S.Logo />
+      <Logo size={200} />
       <RegisterForm />
     </div>
   )
