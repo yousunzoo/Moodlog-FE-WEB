1fae8324a3cb6941610ce3209403c8ef803077c0
dokimion24 <leecy3610@gmail.com>
2023-04-21 21:50:58 +0900

    feat: 불필요한 import 제거

--- src/hooks/useRegisterUser.ts
+++ src/hooks/useRegisterUser.ts
@@ -1,10 +1,6 @@
 import { UseMutateFunction, useMutation, useQueryClient } from 'react-query'
-import { useCookies } from 'react-cookie'
 
-import axios from 'axios'
-import { axiosInstance } from '../apis/axios'
 import { register } from '../apis/auth'
-import { getToken } from '../utils/userTokenCookie'
 
 interface FormData {
   email: string
@@ -14,8 +10,6 @@ interface FormData {
 }
 
 export const useRegisterUser = (): UseMutateFunction<void, unknown, FormData, unknown> => {
-  const queryClient = useQueryClient()
-
   const { mutate } = useMutation((formData: FormData) => register(formData), {})
 
   return mutate
