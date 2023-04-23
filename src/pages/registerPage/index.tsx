23d56da15dd311bd85fe89227e490204ae3c7c78
dokimion24 <leecy3610@gmail.com>
2023-04-23 15:58:50 +0900

    feat: 불필요한 컴포넌트 제거

--- src/pages/registerPage/index.tsx
+++ src/pages/registerPage/index.tsx
@@ -3,7 +3,6 @@ import RegisterForm from '../../components/registerForm'
 import * as S from './style'
 import CloseButton from '../../components/common/button/closeButton'
 import Logo from '../../components/common/logo'
-import Nav from '../../components/common/Nav'
 
 function RegisterPage() {
   return (
@@ -11,7 +10,6 @@ function RegisterPage() {
       <CloseButton />
       <Logo size={200} />
       <RegisterForm />
-      <Nav />
     </S.Wrapper>
   )
 }
