a189fcb1b767b4b21128ca40ca0746925859e8ea
dokimion24 <leecy3610@gmail.com>
2023-04-24 17:47:19 +0900

    design: 스타일 수정

--- src/pages/registerPage/style.ts
+++ src/pages/registerPage/style.ts
@@ -2,4 +2,27 @@ import styled from 'styled-components'
 
 export const Wrapper = styled.div`
   padding: 0 25px;
+  position: relative;
+`
+export const FileButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80px;
+  height: 40px;
+  position: absolute;
+  bottom: 30px;
+  left: 50%;
+  transform: translateX(-50%);
+  border-radius: 10px;
+  background-color: ${({ theme }) => theme.main01};
+  color: ${({ theme }) => theme.buttonText};
+`
+
+export const CloseButton = styled.button`
+  width: 30px;
+  height: 30px;
+  transform: translateY(50%);
+  background-image: url(${({ theme }) => theme.close});
+  background-size: cover;
 `
