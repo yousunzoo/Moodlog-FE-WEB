80a1859618ede77420267f48afaa896fb75407fe
boyon99 <bo_yon_99@naver.com>
2023-04-25 00:12:53 +0900

    feat: own 추가

--- src/hooks/useUserData.ts
+++ src/hooks/useUserData.ts
@@ -1,5 +1,6 @@
 import { useQuery } from 'react-query'
 import { getMyProfile } from '../apis/auth'
+import { useEffect } from 'react'
 
 export default function useUserData() {
   const { data, refetch } = useQuery(['user'], () => getMyProfile(), {
@@ -7,5 +8,14 @@ export default function useUserData() {
     enabled: false,
   })
 
-  return { data, refetch }
+  const { data: own } = useQuery(
+    'own',
+    () =>
+      getMyProfile().then((a) => {
+        return a
+      }),
+    { staleTime: 1000, cacheTime: 100 * 40 },
+  )
+
+  return { data, refetch, own }
 }
