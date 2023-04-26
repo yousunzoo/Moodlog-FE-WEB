ea06d05bdfbbf5b01374675ebde019ed5a84aaa0
boyon99 <bo_yon_99@naver.com>
2023-04-26 15:38:07 +0900

    feat: 주석 추가 및 console.log 제거

--- src/components/follow/follows/index.tsx
+++ src/components/follow/follows/index.tsx
@@ -5,6 +5,9 @@ import { FollowListProp, FollowParent, FollowProp } from '../../../types/follow'
 import Follow from '../follow'
 
 function FollowList({ following, follower, own }: FollowListProp) {
+  // following은 로그인한 유저가 팔로잉하고 있는 유저들의 id 값을 배열로 나타낸 것이며
+  // floower는 팔로잉 되어 있는 유저 정보를 배열 형태로 담은 데이터이며
+  // own은 로그인한 유저의 id 값이다.
   return (
     <S.Follows>
       {follower.map((arr, i) => {
